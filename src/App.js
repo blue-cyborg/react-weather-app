b919b3dd6248fc684230eb82fcc3fc541e2398bb
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-02-24 17:04:51 -0500

    Update App.js
    Made a footer put in my landing page and GitHub url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by {" "}
+        <a href="https://fervent-kare-2cbc33.netlify.app/" 
+        target="_blank" 
+        rel="noopener noreferrer">
+        Candice Drave
+        </a> {" "}and is{" "}
+        <a 
+        href="https://github.com/blue-cyborg/react-weather-app"
+        target="_blank" 
+        rel="noopener noreferrer">
+          open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
