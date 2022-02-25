c22b7394adcd63740c65c162eb27167701371a8d
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-02-25 17:09:12 -0500

    made a css file for Weather

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
       <Weather />
       <footer>
         This project was coded by {" "}
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
     return (
-        <div>Hello from weather</div>
+        <div className="Weather">Hello from weather</div>
         );
 }
