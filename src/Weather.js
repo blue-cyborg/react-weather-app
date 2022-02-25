e105d05b947327a5245a5018f3b522e85e03efd6
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-02-25 17:31:41 -0500

    added layout
    mock data of NY

--- src/Weather.js
+++ src/Weather.js
@@ -3,6 +3,25 @@ import "./Weather.css";
 
 export default function Weather() {
     return (
-        <div className="Weather">Hello from weather</div>
+        <div className="Weather">
+            <h1>New York</h1>
+            <ul>
+                <li>Friday 5:00 PM</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" />
+                    5°C
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Precipitation: 1%</li>
+                        <li>Humidity: 71%</li>
+                        <li>Wind: 8 km/h</li>
+                    </ul>
+                </div>
+                </div>
+            </div>
         );
 }
