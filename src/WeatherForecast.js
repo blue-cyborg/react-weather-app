93f7ab27e5e02bbd128fbc71c0a68e750e1100d6
blue-cyborg <87626094+blue-cyborg@users.noreply.github.com>
2022-03-05 21:32:21 -0500

    Update WeatherForecast.js
    The complex solution worked on my server but failed to deploy. Tried a few times. Decided to try the simple and un elegant option.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,17 +16,23 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-              if (index < 5) {
-                return (
-                  <div className="col" key={index}>
-                    <WeatherForecastDay data={dailyForecast} />
-                  </div>
-                 );
-              }
-            })}
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[1]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[2]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[3]} />
+          </div>
+          <div className="col">
+            <WeatherForecastDay data={forecast[4]} />
           </div>
         </div>
+      </div>
       );
   } else {
     let apiKey = "63b79185d21891dc86fad86aa0ab0b0b";
